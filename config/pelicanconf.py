3c55fece2fa120a81cae66fd1f0b322bad31afb2
Serban Constantin <serban.constantin@freescale.com>
2012-11-20 11:17:46 +0200

    add config files to repository

---
+++ config/pelicanconf.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Serban Constantin'
+SITENAME = u'QWERTY - fast keystrokes'
+SITEURL = 'http://fuzz.me.uk'
+
+TIMEZONE = 'Europe/Bucharest'
+
+DEFAULT_LANG = u'en'
+
+
+DEFAULT_PAGINATION = 5
+
+# old stuff
+FEED_ATOM = ('feeds/all.atom.xml')
+DEFAULT_CATEGORY = ('rand')
+MARKUP = ('rst', 'md')
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TWITTER_USERNAME = 'fuzzmz'
+DISQUS_SITENAME = "qwertyfuzz"
+FEED_DOMAIN = 'http://fuzz.me.uk'
+GOOGLE_ANALYTICS = "UA-28459915-1"
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%b}/{date:%d}/{slug}/index.html'
