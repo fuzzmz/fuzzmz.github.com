577688a6e738afaa577856df81beb89e9d02799f
Serban Constantin <serban.constantin@freescale.com>
2012-11-20 11:28:06 +0200

    move scripts to repo

---
+++ config/scripts/run_scripts.sh
@@ -0,0 +1,7 @@
+pelican -s /home/fuzzmz/pelican/qwerty/content/config/pelicanconf.py /home/fuzzmz/pelican/qwerty/content -o /home/fuzzmz/pelican/qwerty/fuzzmz.github.com -t /home/fuzzmz/pelican/src/pelican/pelican/themes/notmyidea
+git log -1 --pretty=%B > commit.txt
+python replace.py
+chmod +x ./update.sh
+#rm ./commit.txt
+./update.sh
+#rm ./update.sh
