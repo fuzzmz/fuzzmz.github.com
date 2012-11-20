087e666cc1efa03331622a97e9138c8f6d50e6d4
Serban Constantin <serban.constantin@freescale.com>
2012-11-20 11:42:04 +0200

    fix run_script.sh

--- config/scripts/run_scripts.sh
+++ config/scripts/run_scripts.sh
@@ -1,7 +1,8 @@
 pelican -s /home/fuzzmz/pelican/qwerty/content/config/pelicanconf.py /home/fuzzmz/pelican/qwerty/content -o /home/fuzzmz/pelican/qwerty/fuzzmz.github.com -t /home/fuzzmz/pelican/src/pelican/pelican/themes/notmyidea
-git log -1 --pretty=%B > commit.txt
+git --git-dir=/home/fuzzmz/pelican/qwerty/sources/.git --work-tree=/home/fuzzmz/pelican/qwerty/sources log -1 --pretty=%B > commit.txt
 python /home/fuzzmz/pelican/qwerty/content/config/scripts/replace.py
 chmod +x /home/fuzzmz/pelican/qwerty/content/config/scripts/update.sh
 #rm ./commit.txt
 /home/fuzzmz/pelican/qwerty/content/config/scripts/update.sh
 #rm ./update.sh
+
